0c350c99822b77353b3e68fbeea4757723d8c8bd
Arnol-champz <57238530+Arnol-champz@users.noreply.github.com>
2019-11-01 05:54:44 +0700

    Add files via upload

---
+++ RumusPersegi.m
@@ -0,0 +1,7 @@
+clc
+L=input('Enter the length : ');
+B= input('Enter the width:\n');
+p= 2 * (L+B);
+a= L*B;
+fprintf('Perimeter=%d\n',p)
+fprintf('Area=%d\n',a)
---
+++ RumusSegitiga.m
@@ -0,0 +1,9 @@
+clc
+b=input('Enter the value of base : ')
+h=input('Enter the value of height : ')
+hyp=input('Enter the value of Hypotenuse : ')
+area = 0.5*h*b;
+perimeter = b+h+hip;
+
+disp(['the value for area is = ',num2str(area)])
+disp(['the value for perimeter is = ',num2str(perimeter)])
